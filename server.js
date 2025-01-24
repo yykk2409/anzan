5781cadfc681fdf7ec971baeb9cd41fee09b60ad
maetaka-2022066 <yyimymemine3684@gmail.com>
2025-01-24 12:32:21 +0900

    first commit

--- server.js
+++ server.js
@@ -4,13 +4,9 @@ const http = require('http');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
 
 const rooms = {};  // ルームの管理
 let currentAnswer = 0
-
-// HTTPサーバーを作成し、それをSocket.IOに渡す
-const server = http.createServer(app);
 
 // Socket.IOをサーバーに接続
 const io = socketIo(server, {
